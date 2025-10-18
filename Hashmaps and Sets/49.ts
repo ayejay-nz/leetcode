56fe228416531774144f7d515188e51f44e32631
ayejay-nz <55215693+ayejay-nz@users.noreply.github.com>
2025-10-19 07:55:00 +1300

    Solve Q49

---
+++ Hashmaps and Sets/49.ts
@@ -0,0 +1,16 @@
+function groupAnagrams(strs: string[]): string[][] {
+    let anagrams = new Map<string, string[]>();
+
+    // Get all anagrams
+    strs.forEach((str) => {
+        let sortedStr = str.split("").sort().join("");
+
+        if (anagrams.has(sortedStr)) {
+            anagrams.set(sortedStr, [...anagrams.get(sortedStr)!, str]);
+        } else {
+            anagrams.set(sortedStr, [str]);
+        }
+    });
+
+    return [...anagrams.values()];
+}
