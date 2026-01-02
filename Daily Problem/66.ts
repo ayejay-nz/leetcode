5c0dd57a8334d87728670de81d484634a8d2032c
Aidan Webster <55215693+ayejay-nz@users.noreply.github.com>
2026-01-02 15:37:35 +1300

    Solve Q66 daily problem

---
+++ Daily Problem/66.ts
@@ -0,0 +1,9 @@
+function plusOne(digits: number[]): number[] {
+    let num = BigInt(digits.join(""));
+    num++;
+
+    return num
+        .toString()
+        .split("")
+        .map((s) => parseInt(s));
+}
